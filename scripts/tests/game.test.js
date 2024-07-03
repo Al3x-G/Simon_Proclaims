2e7519aa53921fef7fe4c9dad4eef0835a8cbb48
Alex Gordon <alexg1991@outlook.com>
2024-07-03 10:47:19 +0000

    add buttons 1 2 3 4 to test and game js file

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -4,15 +4,16 @@
 
 const { game } = require("../game");
 
+
 beforeAll(() => {
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "utf-8");
     document.open();
     document.write(fileContents);
     document.close();
-})
+});
 
-describe("game object contains correct keys", () =>{
+describe("game object contains correct keys", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
     });
@@ -24,5 +25,8 @@ describe("game object contains correct keys", () =>{
     });
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
